89985da560c369aed35e1bbba1512677b2c156b6
Loorinho <romeojacobson@gmail.com>
2024-05-21 09:26:15 +0300

    decorator modification

--- decorator/Client.java
+++ decorator/Client.java
@@ -3,7 +3,7 @@ public class Client {
    public static void main(String[] args) {
       // Client c = new Client();
 
-      ICalculator cd = new Decorator2(new Decorator2(new Decorator3(new Decorator1())));
+      ICalculator cd = new Decorator3(new Decorator2(new Decorator1()));
 
       int a  = cd.add(4, 6);
       System.out.println(a);
--- decorator/Decorator1.java
+++ decorator/Decorator1.java
@@ -1,4 +1,5 @@
 
+// This is the basic implementation of the calculator. It defines the basic behavior that can be altered by the decorators
 public class Decorator1 implements ICalculator{
 
    @Override
